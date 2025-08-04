15a5ea5a9fc6504272f468a3b03b05c5b49da7da
Albert Alan-Reys <albert.alanreys@gmail.com>
2025-08-04 13:47:44 +1000

    Implement work with workout log

--- src/routes/routes.data.js
+++ src/routes/routes.data.js
@@ -3,8 +3,8 @@ import Home from '../components/screens/home/Home';
 import NewExercise from '../components/screens/new-exercise/NewExercise';
 import NewWorkout from '../components/screens/new-workout/NewWorkout';
 import Profile from '../components/screens/profile/Profile';
-import ListWorkouts from '../components/screens/workout/ListWorkouts';
-import Workout from '../components/screens/workout/Workout';
+import Workout from '../components/screens/workouts/detail/Workout';
+import ListWorkouts from '../components/screens/workouts/list/ListWorkouts';
 
 export const routes = [
 	{
